a8dca3c1f804e1d04f78586c37ac4debf8fb1e7f
Harman061 <107838415+Harman061@users.noreply.github.com>
2023-11-14 11:43:21 +0530

    Time: 12 ms (27.16%) | Memory: 18.7 MB (88.32%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -6,15 +6,15 @@ public:
         if (!i || n > nums[i-1])
             nums[i++] = n;
     return i;*/
-    /*nums.erase(unique(nums.begin(),nums.end()),nums.end());
-    return nums.size();*/
-  int j = 1;
+    nums.erase(unique(nums.begin(),nums.end()),nums.end());
+    return nums.size();
+/*  int j = 1;
         for(int i = 1; i < nums.size(); i++){
             if(nums[i] != nums[i - 1]){
                 nums[j] = nums[i];
                 j++;
             }
         }
-        return j;   
+        return j;   */
 }
 };
