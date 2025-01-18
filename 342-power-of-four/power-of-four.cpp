c632aa13565cb8170c01b9f58e15d4c3a631d12d
Harman061 <107838415+Harman061@users.noreply.github.com>
2025-01-18 18:51:46 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (72.86%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n==0) return false;
-        return (n==1) || ((n%4==0) && (isPowerOfFour(n/4)));
+        /*if(n==0) return false;
+        return (n==1) || ((n%4==0) && (isPowerOfFour(n/4)));*/
+
+        if(n==0) return 0 ;
+        while(n%4==0)
+        {
+            n/=4;
+        }
+        if (n==1) return 1;
+
+        return 0;
     }
 };
