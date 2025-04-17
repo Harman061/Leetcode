5b25b0dcade4c26c07fe2a80bdd27a92d5b18ea6
Harman061 <107838415+Harman061@users.noreply.github.com>
2025-04-17 19:09:04 +0530

    Time: 3 ms (86.58%) | Memory: 14.8 MB (63.61%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        map<char, int> letter;
-        const int sSize = s.size();
-        for (int i = 0; i < sSize; ++i)
-            ++letter[s[i]];
+        vector<int> v (26,0);
+        for(auto it: s)
+        {
+            v[it-'a']++;
+        }
 
-        for (int i = 0; i < sSize; ++i)
-            if (letter[s[i]] == 1)
-                return i;
+        for(int i = 0; i < s.length(); i++){
+			if(v[s[i] - 'a'] == 1) return i;
+		}
         return -1;
     }
 };
