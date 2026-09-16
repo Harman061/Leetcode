d09e1dbbcfc5786e9b08879a3d7e3e4e3122705b
Harman <107838415+Harman061@users.noreply.github.com>
2026-09-16 20:17:58 +0530

    Time: 4 ms (10.62%) | Memory: 71.8 MB (52.26%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,63 +1,20 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        /*int n=nums.size();
-        
-        int max_sum=nums[0];
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i;j<n;j++)
-            {
-                int curr_sum=0;
-                for(int k=i;k<=j;k++)
-                {
-                    curr_sum+=nums[k];
-                    if(curr_sum>max_sum)
-                    {
-                        max_sum=curr_sum;
-                    }
-                    else continue;
-                }
-            }
-        }
-        return max_sum;*/
-
-
-      /*  int n=nums.size();
-        
-        int max_sum=nums[0];
-        for(int i=0;i<n;i++)
-        {
-            int curr_sum=0;
-            for(int j=i;j<n;j++)
-            {
-                
-                    curr_sum+=nums[j];
-                    if(curr_sum>max_sum)
-                    {
-                        max_sum=curr_sum;
-                    }
-                    else continue;
-            }
-        }
-        return max_sum;*/
-
-
-        int curr_sum=0;
-        int max_sum=nums[0];
+        long long sum=LLONG_MIN;
+        long long cSum=0;
         for(int i=0;i<nums.size();i++)
         {
-            curr_sum+=nums[i];
-            if(curr_sum>max_sum)
+            cSum=cSum+nums[i];
+            if(sum<cSum)
             {
-                max_sum=curr_sum;
+                sum=cSum;
             }
-            if(curr_sum<0)
+            if(cSum<0)
             {
-                curr_sum=0;
+                cSum=0;
             }
-           
         }
-        return max_sum;
+        return sum;
     }
 };
